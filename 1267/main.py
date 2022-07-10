9125848dec0c65a5342ff6c8ab1a4ff117de6e12
Sungmin Oh <saogig0503@naver.com>
2022-07-11 02:18:16 +0900

    1267 solved

---
+++ 1267/main.py
@@ -0,0 +1,15 @@
+num = int(input())
+times = list(map(int, input().split()))
+
+Y = 0
+M = 0
+for time in times:
+    Y += ((time // 30) + 1) * 10
+    M += ((time // 60) + 1) * 15
+
+if Y == M:
+    print(f"Y M {Y}")
+elif Y > M:
+    print(f"M {M}")
+else:
+    print(f"Y {Y}")
