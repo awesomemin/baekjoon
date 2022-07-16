fea568edbcf9fb121b0e5adf4f85cb7711e33f23
Sungmin Oh <saogig0503@naver.com>
2022-07-16 13:36:08 +0900

    1247 solved

---
+++ 1247/main.py
@@ -0,0 +1,11 @@
+for TEST_CASE in range(3):
+    N = int(input())
+    sum = 0
+    for _ in range(N):
+        sum += int(input())
+    if sum > 0:
+        print("+")
+    elif sum == 0:
+        print("0")
+    elif sum < 0:
+        print("-")
