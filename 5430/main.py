e12afc6eecd629256d96a63c0820e3f44ab51143
Sungmin Oh <saogig0503@naver.com>
2022-07-18 20:44:44 +0900

    5430 solved

---
+++ 5430/main.py
@@ -0,0 +1,36 @@
+import collections
+import enum
+
+
+T = int(input())
+
+for _ in range(T):
+    is_error = False
+    is_left_head = True
+    commands = input()
+    n = int(input())
+    if n == 0:
+        input()
+        deque = collections.deque()
+    else:
+        deque = collections.deque(list(map(int, input()[1:-1].split(sep=","))))
+    for command in commands:
+        if command == "R":
+            is_left_head = not is_left_head
+        elif command == "D":
+            if len(deque) == 0:
+                is_error = True
+                break
+            else:
+                if is_left_head:
+                    deque.popleft()
+                else:
+                    deque.pop()
+    if is_error:
+        print("error")
+    else:
+        print("[", end="")
+        if is_left_head == False:
+            deque.reverse()
+        print(*deque, sep=",", end="")
+        print("]")
