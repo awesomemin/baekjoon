9e126005affe11453b85b78b6ef2927fc378cae0
awesomemin <saogig0503@naver.com>
2024-03-22 13:56:27 +0000

    15596 solved

---
+++ 15596/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long sum(vector<int> &a) {
+	long long rt = 0;
+	for(int i = 0; i < a.size(); i++) {
+		rt += a[i];
+	}
+	return rt;
+}
