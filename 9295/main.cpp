47d926cfbf5700e91bdf5e28c032e52a95666d63
awesomemin <saogig0503@naver.com>
2024-04-13 06:34:54 +0000

    9295 solved

---
+++ 9295/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int T; cin>>T;
+	for(int i = 1; i <= T; i++) {
+		int A, B; cin>>A>>B;
+		cout<<"Case "<<i<<": "<<A+B<<"\n";
+	}
+	
+	return 0;
+}
